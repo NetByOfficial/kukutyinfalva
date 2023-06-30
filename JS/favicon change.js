852aa4a5f1ccb35e783302a4a925ea03fda85fd0
NetBy <89864425+NetByOfficial@users.noreply.github.com>
2023-06-30 15:49:56 +0200

    Fixálva
    Fixálva

--- JS/favicon change.js
+++ JS/favicon change.js
@@ -1,6 +1,6 @@
 const image_here = document.getElementById('image_here');
 
-  const kepek = ["../Képek/favicon/kukutyinfalva_barna.ico","../Képek/favicon/kukutyinfalva_kek.ico", "../Képek/favicon/kukutyinfalva_lila.ico","../Képek/favicon/kukutyinfalva_vilagoskek.ico"];
+  const kepek = ["Képek/favicon/kukutyinfalva_barna.ico","Képek/favicon/kukutyinfalva_kek.ico", "Képek/favicon/kukutyinfalva_lila.ico","Képek/favicon/kukutyinfalva_vilagoskek.ico"];
   let randomIndex =
     Math.floor(
       Math.random() * kepek.length
