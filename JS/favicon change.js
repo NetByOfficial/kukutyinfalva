53892b6745206c4f072680c7e01d03f7a8c82070
NetBy <89864425+NetByOfficial@users.noreply.github.com>
2023-06-30 15:46:59 +0200

    a

--- JS/favicon change.js
+++ JS/favicon change.js
@@ -1,6 +1,6 @@
 const image_here = document.getElementById('image_here');
 
-  const kepek = ["../Képek/favicon/kukutyinfalva barna.ico","../Képek/favicon/kukutyinfalva kek.ico", "../Képek/favicon/kukutyinfalva lila.ico","../Képek/favicon/kukutyinfalva vilagos kek.ico"];
+  const kepek = ["../Képek/favicon/kukutyinfalva_barna.ico","../Képek/favicon/kukutyinfalva_kek.ico", "../Képek/favicon/kukutyinfalva_lila.ico","../Képek/favicon/kukutyinfalva_vilagoskek.ico"];
   let randomIndex =
     Math.floor(
       Math.random() * kepek.length
