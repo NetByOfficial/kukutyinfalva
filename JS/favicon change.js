742160e321550ff7bf3e72c184100d95dc434974
NetBy <89864425+NetByOfficial@users.noreply.github.com>
2023-06-30 15:42:26 +0200

    bug javitva

--- JS/favicon change.js
+++ JS/favicon change.js
@@ -1,6 +1,6 @@
 const image_here = document.getElementById('image_here');
 
-  const kepek = ["../Képek/favicon/kukutyinfalva barna.png","../Képek/favicon/kukutyinfalva kék.png", "../Képek/favicon/kukutyinfalva lila.png","../Képek/favicon/kukutyinfalva világos kék.png"];
+  const kepek = ["../Képek/favicon/kukutyinfalva barna.ico","../Képek/favicon/kukutyinfalva kek.ico", "../Képek/favicon/kukutyinfalva lila.ico","../Képek/favicon/kukutyinfalva vilagos kek.ico"];
   let randomIndex =
     Math.floor(
       Math.random() * kepek.length
