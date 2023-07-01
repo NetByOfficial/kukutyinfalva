3180334dd2d95fed294e7d07d6011680063af0c8
NetBy <89864425+NetByOfficial@users.noreply.github.com>
2023-07-01 11:00:20 +0200

    Javítva a NAGY "bug"

--- JS/favicon change.js
+++ JS/favicon change.js
@@ -1,10 +1,10 @@
-const image_here = document.getElementById('image_here');
+  const image_here = document.getElementById('image_here');
 
-  const kepek = ["Képek/favicon/kukutyinfalva_barna.ico","Képek/favicon/kukutyinfalva_kek.ico", "Képek/favicon/kukutyinfalva_lila.ico","Képek/favicon/kukutyinfalva_vilagoskek.ico"];
-  let randomIndex =
-    Math.floor(
-      Math.random() * kepek.length
-    );
-  image_here.href = kepek[randomIndex];
+    const kepek_ = ["Képek/favicon/kukutyinfalva_barna.ico","Képek/favicon/kukutyinfalva_kek.ico", "Képek/favicon/kukutyinfalva_lila.ico","Képek/favicon/kukutyinfalva_vilagoskek.ico"];
+    let randomIndex =
+      Math.floor(
+        Math.random() * kepek_.length
+      );
+    image_here.href = kepek_[randomIndex];
 
-  console.log(kepek[randomIndex])
+    console.log(kepek_[randomIndex])
